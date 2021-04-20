c9cb00001dbddecd3a3a8ab75cf92309d6d4ff09
omert7 <32259036+omert7@users.noreply.github.com>
2021-04-20 09:17:01 +0300

    adding missing files

---
+++ src/app/viewModel/AppViewModel.java
@@ -0,0 +1,12 @@
+package app.viewModel;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class AppViewModel implements Observer {
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}
