983fefbc3a30a1b9cda63981252861f2a44edf8b
omert7 <32259036+omert7@users.noreply.github.com>
2021-05-07 22:22:54 +0300

    adding setting.json and settings loader

--- src/app/tests/Tests.java
+++ src/app/tests/Tests.java
@@ -1,6 +1,8 @@
 package app.tests;
 
-import app.model.CsvFlightHandler;
+//import app.model.CsvFlightHandler;
+import app.model.DefaultXmlGen;
+import app.model.FlightSettings;
 
 public class Tests {
 
@@ -10,11 +12,15 @@ public class Tests {
 //        for (CorrelationType ct : ts.dataCoral) {
 //            System.out.println(ct.CoralA + " + " + ct.CoralB + " : " + ct.type);
 //        }
-        CsvFlightHandler cfh = new CsvFlightHandler("C:\\Users\\tatio\\patam2\\src\\files\\anomaly_flight.csv");
-        cfh.readCsvFeatures();
-        cfh.CreateListOfFeaturesFromCsvColumns();
-        cfh.serializeToXML();
-        System.out.println(cfh.getCsvColumns());
+//        CsvFlightHandler cfh = new CsvFlightHandler("C:\\Users\\tatio\\patam2\\src\\files\\anomaly_flight.csv");
+//        cfh.readCsvFeatures();
+//        cfh.CreateListOfFeaturesFromCsvColumns();
+//        cfh.serializeToXML();
+//        System.out.println(cfh.getCsvColumns());
+        String settingsFile = "C:\\Users\\tatio\\patam2\\src\\files\\settings.json";
+        FlightSettings fs = new FlightSettings(settingsFile);
+        fs.loadSettings();
+
     }
 
 }
--- src/app/view/AppView.java
+++ src/app/view/AppView.java
@@ -1,4 +1,28 @@
 package app.view;
 
-public class AppView {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import static javafx.application.Application.launch;
+
+
+public class AppView extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("appView.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+    public void printf(){
+        System.out.println("OMER!!!");
+    }
+    public static void main(String[] args) {
+        {
+            launch(args);
+        }
+    }
 }
