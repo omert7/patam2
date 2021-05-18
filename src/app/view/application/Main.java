788e2ab9d74ccbd2a8ce0fabc172b8de732bf3ba
omert7 <32259036+omert7@users.noreply.github.com>
2021-05-18 10:34:09 +0300

    updating paths

--- src/app/view/application/Main.java
+++ src/app/view/application/Main.java
@@ -5,7 +5,6 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
- 
 
 public class Main extends Application {
 	@Override
@@ -13,7 +12,7 @@ public class Main extends Application {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
 			Scene scene = new Scene(root,1400,680);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getResource("./application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
