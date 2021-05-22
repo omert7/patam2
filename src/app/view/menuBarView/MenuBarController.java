008e3dca216713ffbaf86c52714e0fb4fca8e329
DESKTOP-NFKEKDB\guylevy <guylevy2307@gmail.com>
2021-05-22 15:12:34 +0300

    view chainging

--- src/app/view/menuBarView/MenuBarController.java
+++ src/app/view/menuBarView/MenuBarController.java
@@ -1,6 +1,11 @@
 package app.view.menuBarView;
+import java.io.File;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 public class MenuBarController {
 	@FXML private MenuItem setttingXml;
@@ -17,4 +22,37 @@ public class MenuBarController {
     {
     	//
     }
+    @FXML
+     void xmlFileChooser(ActionEvent event) 
+    {
+    	FileChooser fc=new FileChooser();
+    	fc.getExtensionFilters().add(new ExtensionFilter("xml files", "*.xml"));
+    	File f=fc.showOpenDialog(null);
+    	if (f!=null)
+    	{
+    		System.out.println(f.getName());
+    	}
+    }
+    @FXML
+    void csvFileChooser(ActionEvent event) 
+   {
+    	FileChooser fc=new FileChooser();
+    	fc.getExtensionFilters().add(new ExtensionFilter(" csv files", "*.csv"));
+    	File f=fc.showOpenDialog(null);
+    	if (f!=null)
+    	{
+    		System.out.println(f.getName());
+    	}
+   }
+    @FXML
+    void algoFileChooser(ActionEvent event) 
+   {
+    	FileChooser fc=new FileChooser();
+    	fc.getExtensionFilters().add(new ExtensionFilter("Anomaly detector files", "*.java"));
+    	File f=fc.showOpenDialog(null);
+    	if (f!=null)
+    	{
+    		System.out.println(f.getName());
+    	}
+   }
 }
